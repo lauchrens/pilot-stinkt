da67e395e26214699c81b36db116e9c71431def4
lauchrens <104091795+lauchrens@users.noreply.github.com>
2023-04-15 17:05:09 +0200

    joa

---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="home-content">
+            <h1>Es ist mir eine Pflicht zu berichten, dass der Pilot einen unangenehmen Geruch aus dem Mundbereich aufweist.</h1>
+        </div>
+    )
+}
+
+export default Home;
